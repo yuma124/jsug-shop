17ce2139fd6af00cef2f2dafcfe7267d66e9a51b
Yuma Sasahara <yuma.sasahara@gmail.com>
2016-10-19 22:54:23 +0900

    create LoginController

---
+++ src/main/java/jsug/app/login/LoginController.java
@@ -0,0 +1,17 @@
+package jsug.app.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by yuma on 2016/10/19.
+ */
+@Controller
+public class LoginController {
+
+    @RequestMapping("loginForm")
+    String loginForm() {
+        return "login/loginForm";
+    }
+
+}
