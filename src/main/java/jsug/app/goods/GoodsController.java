088b656aab3c9697fa000b22374fe1bd574c3faa
yuma124 <y_sasahara@r.recruit.co.jp>
2016-10-21 17:48:24 +0900

    add exception handler to GoodsController

--- src/main/java/jsug/app/goods/GoodsController.java
+++ src/main/java/jsug/app/goods/GoodsController.java
@@ -5,19 +5,18 @@ import jsug.domain.model.Category;
 import jsug.domain.model.Goods;
 import jsug.domain.model.OrderLine;
 import jsug.domain.service.category.CategoryService;
+import jsug.domain.service.goods.GoodsNotFoundException;
 import jsug.domain.service.goods.GoodsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -60,6 +59,12 @@ public class GoodsController {
         Goods goods = goodsService.findOne(form.getGoodsId());
         cart.add(OrderLine.builder().goods(goods).quantity(form.getQuantity()).build());
         return "redirect:/cart";
+    }
+
+    @ExceptionHandler(GoodsNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    String handleGoodsNotFoundException() {
+        return "goods/notFound";
     }
 
 }
