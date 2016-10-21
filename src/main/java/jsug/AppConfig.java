471e51621a6545f3ce08c9a1fcdf58492f0ebbbc
yuma124 <y_sasahara@r.recruit.co.jp>
2016-10-21 12:58:11 +0900

    create GoodsController

--- src/main/java/jsug/AppConfig.java
+++ src/main/java/jsug/AppConfig.java
@@ -1,13 +1,13 @@
 package jsug;
 
+import jsug.domain.model.Cart;
 import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.*;
+import org.springframework.web.context.WebApplicationContext;
 
 import javax.sql.DataSource;
 
@@ -33,5 +33,11 @@ public class AppConfig {
     @Bean
     DataSource dataSource() {
         return new DataSourceSpy(this.dataSource);
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_SESSION, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    Cart cart() {
+        return new Cart();
     }
 }
