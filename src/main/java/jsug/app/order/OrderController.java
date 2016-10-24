d834be13593901b74b55efe6d36965f3ce08ffbe
yuma124 <y_sasahara@r.recruit.co.jp>
2016-10-24 10:25:07 +0900

    add exception handler to OrderController

--- src/main/java/jsug/app/order/OrderController.java
+++ src/main/java/jsug/app/order/OrderController.java
@@ -2,16 +2,18 @@ package jsug.app.order;
 
 import jsug.domain.model.Cart;
 import jsug.domain.model.Order;
+import jsug.domain.service.order.EmptyCartOrderException;
+import jsug.domain.service.order.InvalidCartOrderException;
 import jsug.domain.service.order.OrderService;
 import jsug.domain.service.userdetails.ShopUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
@@ -48,6 +50,12 @@ public class OrderController {
     @RequestMapping(method = RequestMethod.GET, params = "finish")
     String finish() {
         return "order/finish";
+    }
+
+    @ExceptionHandler({EmptyCartOrderException.class, InvalidCartOrderException.class})
+    @ResponseStatus(HttpStatus.CONFLICT)
+    ModelAndView handleOrderException(RuntimeException e) {
+        return new ModelAndView("order/error").addObject("error", e.getMessage());
     }
 
 }
